ff5cc0098ab5af40a9d4eb9b08eb26446b71ed28
Filip <filipsarovic428@gmail.com>
2023-03-22 17:35:56 +0100

    Finished with make the Player moves

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function () {
 
   ctx.fillStyle = 'white';
   ctx.lineWidth = 3;
-  ctx.strokeStyle = 'white';
+  ctx.strokeStyle = 'purple';
 
   class Player {
     constructor(game) {
@@ -14,16 +14,44 @@ window.addEventListener('load', function () {
       this.collisionX = this.game.width * 0.5;
       this.collisionY = this.game.height * 0.5;
       this.collisionRadius = 30;
+      this.speedX = 0;
+      this.speedY = 0;
+      this.dx = 0;
+      this.dy = 0;
+      this.speedModifier = 20;
     }
     draw(context) {
-      context.beginPath(); // to draw circle canvas
-      // built-in "arc" method which expects 5 args(X and Y coordinates, RADIUS of circle, START and END angle in radians)
+      // to draw circle canvas
+      context.beginPath();
+      /**
+       * built-in "arc" method which expects 5 args
+       * (X and Y coordinates, RADIUS of circle, START and END angle in radians)
+       */
       context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+
       context.save();
       context.globalAlpha = 0.5;
       context.fill();
       context.restore();
       context.stroke();
+      context.beginPath();
+      context.moveTo(this.collisionX, this.collisionY);
+      context.lineTo(this.game.mouse.x, this.game.mouse.y);
+      context.stroke();
+    }
+    update() {
+      this.dx = this.game.mouse.x - this.collisionX;
+      this.dy = this.game.mouse.y - this.collisionY;
+      const distance = Math.hypot(this.dy, this.dx);
+      if (distance > this.speedModifier) {
+        this.speedX = this.dx / distance || 0;
+        this.speedY = this.dy / distance || 0;
+      } else {
+        this.speedX = 0;
+        this.speedY = 0;
+      }
+      this.collisionX += this.speedX * this.speedModifier;
+      this.collisionY += this.speedY * this.speedModifier;
     }
   }
 
@@ -51,21 +79,28 @@ window.addEventListener('load', function () {
         this.mouse.pressed = false
       });
       canvas.addEventListener('mousemove', e => {
-        this.mouse.x = e.offsetX;
-        this.mouse.y = e.offsetY;
-        console.log(this.mouse.x);
+        if (this.mouse.pressed) {
+          this.mouse.x = e.offsetX;
+          this.mouse.y = e.offsetY;
+        }
       })
     }
     render(context) {
       this.player.draw(context);
+      this.player.update();
     }
   }
 
   const game = new Game(canvas);
-  game.render(ctx);
-  console.log(game);
 
   function animate() {
-
+    /**
+     * here we used built-in clear rectangle method to clear old paint,
+     * clear all canvas area with coord. 0, 0 to canvas width and height
+     */
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(ctx);
+    requestAnimationFrame(animate);
   }
+  animate();
 });
